473a4bb9d30843b49641922f0b89e552490fd796
mitul72 <mituldhawan2@gmail.com>
2023-11-30 14:20:26 -0700

    Add vulkan_init function and swapchain

--- src/vk_engine.cpp
+++ src/vk_engine.cpp
@@ -1,11 +1,24 @@
-﻿
-#include "vk_engine.h"
+﻿#include "vk_engine.h"
 
 #include <SDL.h>
 #include <SDL_vulkan.h>
 
 #include <vk_types.h>
 #include <vk_initializers.h>
+
+// bootstrap library
+#include "VkBootstrap.h"
+
+#define VK_CHECK(x)                                                     \
+	do                                                                  \
+	{                                                                   \
+		VkResult err = x;                                               \
+		if (err)                                                        \
+		{                                                               \
+			std::cout << "Detected Vulkan error: " << err << std::endl; \
+			abort();                                                    \
+		}                                                               \
+	} while (0)
 
 void VulkanEngine::init()
 {
@@ -22,9 +35,53 @@ void VulkanEngine::init()
 		_windowExtent.height,
 		window_flags);
 
+	// load the core Vulkan structures
+	init_vulkan();
+
 	// everything went fine
 	_isInitialized = true;
 }
+
+void VulkanEngine::init_vulkan()
+{
+	vkb::InstanceBuilder builder;
+
+	// make the Vulkan instance, with basic debug features
+	auto inst_ret = builder.set_app_name("Example Vulkan Application")
+						.request_validation_layers(true)
+						.require_api_version(1, 1, 0)
+						.use_default_debug_messenger()
+						.build();
+
+	vkb::Instance vkb_inst = inst_ret.value();
+
+	// store the instance
+	_instance = vkb_inst.instance;
+	// store the debug messenger
+	_debug_messenger = vkb_inst.debug_messenger;
+
+	// get the surface of the window we opened with SDL
+	SDL_Vulkan_CreateSurface(_window, _instance, &_surface);
+
+	// use vkbootstrap to select a GPU.
+	// We want a GPU that can write to the SDL surface and supports Vulkan 1.1
+	vkb::PhysicalDeviceSelector selector{vkb_inst};
+	vkb::PhysicalDevice physicalDevice = selector
+											 .set_minimum_version(1, 1)
+											 .set_surface(_surface)
+											 .select()
+											 .value();
+
+	// create the final Vulkan device
+	vkb::DeviceBuilder deviceBuilder{physicalDevice};
+
+	vkb::Device vkbDevice = deviceBuilder.build().value();
+
+	// Get the VkDevice handle used in the rest of a Vulkan application
+	_device = vkbDevice.device;
+	_chosenGPU = physicalDevice.physical_device;
+}
+
 void VulkanEngine::cleanup()
 {
 	if (_isInitialized)
--- src/vk_engine.h
+++ src/vk_engine.h
@@ -5,25 +5,47 @@
 
 #include <vk_types.h>
 
-class VulkanEngine {
+class VulkanEngine
+{
 public:
+	// swapchain
+	VkSwapchainKHR _swapchain; // from other articles
 
-	bool _isInitialized{ false };
-	int _frameNumber {0};
+	// image format expected by the windowing system
+	VkFormat _swapchainImageFormat;
 
-	VkExtent2D _windowExtent{ 1700 , 900 };
+	// array of images from the swapchain
+	std::vector<VkImage> _swapchainImages;
 
-	struct SDL_Window* _window{ nullptr };
+	// array of image-views from the swapchain
+	std::vector<VkImageView> _swapchainImageViews;
 
-	//initializes everything in the engine
+	VkInstance _instance;					   // Vulkan library handle
+	VkDebugUtilsMessengerEXT _debug_messenger; // Vulkan debug output handle
+	VkPhysicalDevice _chosenGPU;			   // GPU chosen as the default device
+	VkDevice _device;						   // Vulkan device for commands
+	VkSurfaceKHR _surface;					   // Vulkan window surface
+
+	bool _isInitialized{false};
+	int _frameNumber{0};
+
+	VkExtent2D _windowExtent{850, 450};
+
+	struct SDL_Window *_window{nullptr};
+
+	// initializes everything in the engine
 	void init();
 
-	//shuts down the engine
+	// shuts down the engine
 	void cleanup();
 
-	//draw loop
+	// draw loop
 	void draw();
 
-	//run main loop
+	// run main loop
 	void run();
+
+private:
+	void init_vulkan();
+	void init_swapchain();
 };
